c073a119cc506473b32fc829f0f9142b05b8cab1
tuanpq1998 <tuanquang123321@gmail.com>
2020-07-28 22:11:03 +0700

    add react front end

---
+++ src/main/resources/static/precache-manifest.63d8bbb16226b1cc70c519fafad1d91e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9369860fd5f98a6860ec9adfc25c295",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e129fa447c120c5ab084",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4c3403397e2669335563",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e129fa447c120c5ab084",
+    "url": "/static/js/2.57c85220.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.57c85220.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c3403397e2669335563",
+    "url": "/static/js/main.c93f6287.chunk.js"
+  },
+  {
+    "revision": "6e4c2c0b1ab5a84e8a7c",
+    "url": "/static/js/runtime-main.79a2ba3b.js"
+  }
+]);
